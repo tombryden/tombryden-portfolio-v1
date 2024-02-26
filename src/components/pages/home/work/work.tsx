6869c117ebe73f06779fafd11220e221847e3df3
Tom <tombryden@me.com>
2024-02-26 15:23:26 +0000

    Updated experience

--- src/components/pages/home/work/work.tsx
+++ src/components/pages/home/work/work.tsx
@@ -6,9 +6,9 @@ const IBMPoints = [
   "Underwent an intensive degree apprenticeship at IBM for my first four years, allocating four days weekly to client work and one day to study.",
   "Achieved a First-Class Honours degree in Digital & Technology Solutions (Software Engineering Pathway).",
   "Smoothly transitioned to a full-time role within IBM's UKI Quality Engineering Practice.",
+  "Managed multiple teams through high pressure test phases.",
   "Acquired broad experience in various industries.",
   "Fulfilled diverse testing duties, enhancing skills in API stubbing, automation suite development and more.",
-  "Exhibited flexibility and proficiency in managing intricate testing scenarios and teams.",
   "Refined technical skills and deepened knowledge of sector-specific challenges.",
   "Emerged as a highly adaptable and essential contributor to the software testing and development sphere.",
 ];
