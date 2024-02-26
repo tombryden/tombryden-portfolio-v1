1a13b11359133c524edd5d0308c444d7aeaf5f58
Tom <tombryden@me.com>
2024-02-26 02:49:13 +0000

    Uncommented metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import AboutMe from "@/components/pages/home/about-me/about-me";
 import Greeting from "@/components/pages/home/greeting/greeting";
 import SideProjects from "@/components/pages/home/side-projects/side-projects";
 import Work from "@/components/pages/home/work/work";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Tom Bryden - Portfolio",
-//   description:
-//     "I harness the transformative power of programming and technology to drive innovation and deliver impactful solutions. With a rich skill set I craft dynamic, scalable digital solutions that meets the demands of today's world.",
-// };
+export const metadata: Metadata = {
+  title: "Tom Bryden - Portfolio",
+  description:
+    "I harness the transformative power of programming and technology to drive innovation and deliver impactful solutions. With a rich skill set I craft dynamic, scalable digital solutions that meets the demands of today's world.",
+};
 
 export default function Home() {
   return (
