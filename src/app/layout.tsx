c63c10b9f68f9ae03be3718f8164796b320fc924
Tom <tombryden@me.com>
2024-08-27 13:38:50 +0100

    Ref: reduced metadata for better description on search engines + changed email

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Tom Bryden - Portfolio",
-  description:
-    "I harness the transformative power of programming and technology to drive innovation and deliver impactful solutions. With a rich skill set I craft dynamic, scalable digital solutions that meets the demands of today's world.",
-};
+// export const metadata: Metadata = {
+//   title: "Tom Bryden - Portfolio",
+//   description:
+//     "I harness the transformative power of programming and technology to drive innovation and deliver impactful solutions. With a rich skill set I craft dynamic, scalable digital solutions that meets the demands of today's world.",
+// };
 
 export default function RootLayout({
   children,
